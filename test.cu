23963705bbca8d6730f263271524ac260875c812
akshat-sj <116817944+akshat-sj@users.noreply.github.com>
2025-01-04 19:43:30 +0530

    naive implementation of v2

--- test.cu
+++ test.cu
@@ -2,7 +2,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-__global__ void forward_kernel_basic(const float* Q,const float* K,const float* V,const int N,const int d,const int Tc,const int Tr, const int Bc,const int Br,const float scale,float* l,float* m,float* O){
+__global__ void forward_kernel_v1(const float* Q,const float* K,const float* V,const int N,const int d,const int Tc,const int Tr, const int Bc,const int Br,const float scale,float* l,float* m,float* O){
     int tid = threadIdx.x;
     int bid_x = blockIdx.x;
     int bid_y = blockIdx.y;
@@ -72,41 +72,84 @@ __global__ void forward_kernel_basic(const float* Q,const float* K,const float* 
 
 }
 
-template <int Br, int Bc, int d>
-__global__ void forward_kernel(
-    const float* Q, const float* K, const float* V, 
-    const int N, const int d, const int Tc, const int Tr, 
-    const int Bc, const int Br,const int batch_stride, const float scale, 
-    float* l, float* m, float* O) {
-    
-    int tid_x = threadIdx.x;
-    int tid_y = threadIdx.y;
-    int batch_offset = blockIdx.x * batch_stride; // Batch-specific offset
+__global__ void forward_kernel_v2(const float* Q,const float* K,const float* V,const int N,const int d,const int Tc,const int Tr, const int Bc,const int Br,const float scale,float* l,float* m,float* O){
+    int tid = threadIdx.x;
+    int bid_x = blockIdx.x;
+    int bid_y = blockIdx.y;
 
-    __shared__ float Qi[Br][d]; 
-    __shared__ float Kj[Br][Bc];
-    __shared__ float Vj[Br][d];
+    int qkv_offset = (bid_x * gridDim.y * N * d) + (bid_y * N * d);
+    int lm_offset = (bid_x * gridDim.y * N) + (bid_y * N);
 
-    __shared__ float Si[Br][Bc];
 
-    const int num_tiles = d/Bc; 
+    extern __shared__ float sram[];
 
-    float Oi[num_tiles];
-    float li = 0.0f;
-    float mi = - INFINITY;
-    for(int x = 0;x<num_tiles;x++){
-        Oi[x] = 0;
-        Qi[tid_y][x*Bc + tid_x] = Q[batch_offset + (blockIdx.y * Br + tid_y)*d +(x*Bc + tid_x)];
-    }
-    __syncthreads();
+    int tile_size = Bc*d;
 
-    for(int j=0;j<Tc;j++){
+    float* Qi = sram;
+    float* Kj = &sram[tile_size];
+    float* Vj = &sram[tile_size*2];
+    float* S = &sram[tile_size*3];
+
+   for(int i = 0;i<Tr;i++){
+        for(int x=0;x<d;x++){
+            Qi[(tid*d)+x] = Q[qkv_offset + (tile_size*i) + (tid*d)+x];
+        }
+        float row_m_prev = m[lm_offset + (Br*i) + tid];
+        float row_l_prev = l[lm_offset + (Br*i) + tid];
+        __syncthreads();
+        float row_l_new;
+        float row_m_new;
+
+        for(int j=0;j<Tc;j++){
+        for(int x=0;x<d;x++){
+            Kj[(tid*d)+x] = K[qkv_offset + (tile_size*j) + (tid*d)+x];
+            Vj[(tid*d)+x] = V[qkv_offset + (tile_size*j) + (tid*d)+x];
+        }
         
+        float row_m = -INFINITY;
+        for(int y=0;y<Bc;y++){
+            float sum = 0;
+            for(int x = 0;x<d;x++){
+                sum+=  Qi[(tid*d)+x] * Kj[(y*d)+x];
+            }
+            sum*=scale;
+            S[(Bc*tid) + y] = sum;
+            if(sum>row_m){
+                row_m = sum;
+                }
+        }
+            
+        row_m_new = max(row_m_prev,row_m);
+        float row_l = 0;
+        for(int y =0;y<Bc;y++){
+            S[(Bc * tid) + y] = __expf(S[(Bc * tid) + y] - row_m_new);
+            row_l +=  S[(Bc * tid) + y];
+            }
+        row_l_new = (__expf(row_m_prev - row_m_new) * row_l_prev) + row_l;
+
+        for(int x = 0; x < d;x++){
+                float pv = 0;
+                for(int y=0;y<Bc;y++){
+                    pv += S[(Bc * tid) + y] * Vj[(y*d)+x];
+                }
+                O[qkv_offset + (tile_size*i) + (tid*d)+x] = (__expf(row_m_prev - row_m_new) * O[qkv_offset + (tile_size * i) + (tid * d) + x]+pv);
+            }
+        
+        row_m_prev = row_m_new;
+        row_l_prev = row_l_new;
+        }
+        __syncthreads();
+        for (int x = 0; x < d; x++)
+            O[qkv_offset + (tile_size * i) + (tid * d) + x] *= 1/row_l_new;
+
+        l[lm_offset + (Br * i) + tid] = row_m_new + __logf(row_l_new);
+
     }
-
-
-
 }
+
+
+
+
 
 torch::Tensor forward(torch::Tensor Q,torch::Tensor K, torch::Tensor V){
     const int Bc = 32;
@@ -137,7 +180,7 @@ torch::Tensor forward(torch::Tensor Q,torch::Tensor K, torch::Tensor V){
     dim3 grid_dim(B,nh);
     dim3 block_dim(Bc);
 
-    forward_kernel_basic<<<grid_dim,block_dim,sram_size>>>(Q.data_ptr<float>(), K.data_ptr<float>(), V.data_ptr<float>(),
+    forward_kernel_v2<<<grid_dim,block_dim,sram_size>>>(Q.data_ptr<float>(), K.data_ptr<float>(), V.data_ptr<float>(),
         N, d, Tc, Tr, Bc, Br, scale,
         l.data_ptr<float>(), m.data_ptr<float>(), O.data_ptr<float>());
 
